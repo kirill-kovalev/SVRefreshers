d9da393af63abb398acb80e09e17c7e1edc8a26b
Kirill Kovalev <kirilkovalev@yandex.ru>
2021-10-12 12:37:23 +0300

    Initial Commit

---
+++ ScrollViewRefreshers/ScrollViewRefreshers.h
@@ -0,0 +1,18 @@
+//
+//  ScrollViewRefreshers.h
+//  ScrollViewRefreshers
+//
+//  Created by Кирилл on 12.10.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ScrollViewRefreshers.
+FOUNDATION_EXPORT double ScrollViewRefreshersVersionNumber;
+
+//! Project version string for ScrollViewRefreshers.
+FOUNDATION_EXPORT const unsigned char ScrollViewRefreshersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScrollViewRefreshers/PublicHeader.h>
+
+
